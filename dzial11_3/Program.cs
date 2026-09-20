76d0c3ce9ed30141fe9db23c6102803765ea91f5
baqixon <baqixon@gmail.com>
2026-09-20 20:22:04 +0200

    Dodaj pliki projektów.

---
+++ dzial11_3/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+class KontoBankowe
+{
+    private double saldo;
+    public double Saldo
+    {
+        get { return saldo; }
+        private set { saldo = value; }
+    }
+    public KontoBankowe()
+    {
+        saldo = 0;
+    }
+    public KontoBankowe(double saldoPoczatkowe)
+    {
+        saldo = saldoPoczatkowe >= 0 ? saldoPoczatkowe : 0;
+    }
+    public void Wplac(double kwota)
+    {
+        if (kwota > 0)
+        {
+            saldo += kwota;
+            Console.WriteLine($"Wpłacono: {kwota} zł.");
+        }
+        else
+        {
+            Console.WriteLine("Kwota wpłaty musi być dodatnia!");
+        }
+    }
+
+
+    public void Wyplac(double kwota)
+    {
+        if (kwota > 0 && kwota <= saldo)
+        {
+            saldo -= kwota;
+            Console.WriteLine($"Wypłacono: {kwota} zł.");
+        }
+        else if (kwota > saldo)
+        {
+            Console.WriteLine($"Nie powiodła się wypłata {kwota} zł: Brak wystarczających środków na koncie!");
+        }
+        else
+        {
+            Console.WriteLine("Kwota wypłaty musi być dodatnia!");
+        }
+    }
+
+
+    public void PokazSaldo()
+    {
+        Console.WriteLine($"Aktualny stan konta: {saldo} zł\n");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("--- Testowanie konta z konstruktorem domyślnym ---");
+        KontoBankowe konto1 = new KontoBankowe();
+        konto1.PokazSaldo();
+        Console.WriteLine("Test wpłaty");
+        konto1.Wplac(500);
+        konto1.PokazSaldo();
+        Console.WriteLine("Test poprawnej wypłaty");
+
+        konto1.Wyplac(200);
+        konto1.PokazSaldo();
+
+        Console.WriteLine("Test próby wypłaty kwoty większej niż sald");
+        konto1.Wyplac(400); // Saldo wynosi teraz 300, więc wypłata 400 zł się nie uda
+        konto1.PokazSaldo();
+
+        Console.WriteLine("Test konta z konstruktorem z saldem początkowym");
+        KontoBankowe konto2 =new KontoBankowe(1000);
+        konto2.PokazSaldo();
+
+         
+    }
+}
---
+++ dzial13_3/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+
+class Liczby
+{
+    List<int> liczby = new List<int> { };
+    public bool koniec = false;
+    public string podana_liczba { protected get;  set; }
+    public int liczba;
+    public void WyswietlLiczby()
+    {
+        int ilosc = 0;
+        int suma = 0;
+        Console.WriteLine("Podane liczby:");
+        foreach (int liczba in liczby)
+        {
+            Console.WriteLine(liczba);
+            ilosc++;
+            suma += liczba;
+        }
+        Console.WriteLine($"Liczba ocen: {ilosc}\n A ich średnia to   {(double)suma / ilosc:F2}");
+    }
+    public void wpiszLiczbe()
+    {
+        bool isNumber;
+        while (true)
+        {
+            Console.WriteLine("Podaj ocenę 1-6 (lub napisz 'koniec' aby zakończyć):");
+            podana_liczba = Console.ReadLine();
+            isNumber = int.TryParse(podana_liczba, out liczba);
+            if (podana_liczba.ToLower() == "koniec")
+            {
+                koniec = true;
+            }
+            else if (isNumber && liczba >= 1 && liczba <= 6)
+            {
+                liczby.Add(liczba);
+            }
+            else
+            {
+                Console.WriteLine("Niepoprawna ocena. Proszę podać liczbę z zakresu 1-6 lub napisać 'koniec' aby zakończyć.");
+            }
+
+
+
+
+
+            if (koniec)
+            {
+                break;
+            }
+        }
+    }
+
+
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Liczby liczby = new Liczby();
+        liczby.wpiszLiczbe();
+        liczby.WyswietlLiczby();
+
+
+    } 
+}
